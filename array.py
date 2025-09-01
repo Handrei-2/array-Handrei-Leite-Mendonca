35cf143d5b7bc01c22b7ccfc9d29303191a1abd2
Handrei-2 <handreimendonca5@gmail.com>
2025-09-01 19:21:19 -0300

    array.py

---
+++ array.py
@@ -0,0 +1,9 @@
+alunos = ["Fernando", "Ítalo", "Kauan", "Marcos", "José"]
+print(alunos[0])
+print(alunos[2])
+print(alunos[-1])
+if "Carla" in alunos:
+    print("Carla está na lista")
+else:
+    print("Carla não está na lista")
+
